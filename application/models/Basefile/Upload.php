953538bf121d60cf84299d6c184ed7f8eea694a3
rabotaresp <36397055+rabotaresp@users.noreply.github.com>
2019-04-22 11:02:10 +0300

    fix

--- application/models/Basefile/Upload.php
+++ application/models/Basefile/Upload.php
@@ -1,10 +1,6 @@
 <?php
 class Upload extends CI_Model {
 
-	public $name;
-	public $ext;
-	public $path;
-	public $linkkey;
 
 	public function __construct()
 	{
@@ -13,20 +9,22 @@ class Upload extends CI_Model {
 		$this->load->library('session');
 	}
 	public function uploadfile($uname, $unametmp){
-		$this->$name = md5(time().rand(1,99).$this->$uname);
-		$this->$ext = explode('.', $this->$uname);
-		$this->$ext = $this->$ext[count($this->$ext)-1];
-		$this->$path = './uploads/'.$name[0].'/'.$name[1];
-		$this->$linkkey = md5(time().rand(1,9));
+		$name = md5(time().rand(1,99).$uname);
+		$ext = explode('.', $uname);
+		$ext = $ext[count($ext)-1];
+		$path = './uploads/'.$name[0].'/'.$name[1];
+		$linkkey = md5(time().rand(1,9));
 		if(!file_exists($path)){
 					mkdir($path, 0777,true);
-				}
-		move_uploaded_file($this->$unametmp, $this->$path.'/'.$this->$name.'.'.$this->$ext);
-		$datafiles = [
-					'Id_User' => $_SESSION['id'],
-					'Link' => $this->$path.'/'.$this->$name.'.'.$this->$ext,
-					'Link_Key' => $this->$linkkey
-				];
-				$this->db->insert('files', $datafiles);
+		}
+		if(move_uploaded_file($unametmp, $path.'/'.$name.'.'.$ext)){
+            $datafiles = [
+                'Id_User' => $_SESSION['id'],
+                'Link' => $path.'/'.$name.'.'.$ext,
+                'Link_Key' => $linkkey
+            ];
+            $this->db->insert('files', $datafiles);
+        }
+
 	}
 }
